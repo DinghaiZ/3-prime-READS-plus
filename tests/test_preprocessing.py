c2771be4d58fa7069bf51909fd521d0ddf81ced7
DinghaiZ <dinghaiz@gmail.com>
2019-02-02 08:56:18 -0500

    Added trim_5p_Ts() method to the Fastq class.

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -9,7 +9,41 @@ import PASS
 
 def test_fastq_reader():
     infile = "data/rawfastq/siCtrl_1_small.fastq"
-    for i, fastq_record in enumerate(PASS.fastq_reader(infile)):
-        if i == 0:
+    for i, fastq_record in enumerate(PASS.fastq_reader(infile, 6, 4)):
+        if i == 1:
+            assert fastq_record.randNT5 == 6
+
             assert fastq_record.name == \
-                "NB500952:186:H35F2BGX5:1:11101:6050:1064 1:N:0:ATCACG"
+                "NB500952:186:H35F2BGX5:1:11101:14875:1080 1:N:0:ATCACG"
+            assert fastq_record.seq == \
+                "TATCTCTTTTATTTTTTTTTTTTGAAGGGCAGATTTAAAATACACTATTAAAATTATTAAATATTAAAAAACAACA"
+            assert fastq_record.qual == \
+                "AAA/A/EEEEEEEEEEEEEAEE/AAE///E///E</EE/A/EE</6//6//<66//6//6//////<//6EE/6/6"
+    
+    assert i == 249
+
+def test_trim_5p_Ts():
+    infile = "data/rawfastq/siCtrl_1_small.fastq"
+    for i, fastq_record in enumerate(PASS.fastq_reader(infile, 6, 4)):
+        # Trim once
+        if i == 2:
+            fastq_record.trim_5p_Ts()
+            assert fastq_record.name == \
+                "TS16AACCAT:NB500952:186:H35F2BGX5:1:11101:13764:1083 1:N:0:ATCACG"
+            assert fastq_record.seq == \
+                "AAAAAAAAAATAAAAAATATTTTTAAAATTATAAATTTTTTTTTGTAAAATCAC"
+            assert fastq_record.qual == \
+                "EEEAEE/E///6AEE////A//<<6//66//6//66//66/6/6////////66"
+        # Trim twice
+        if i == 1:
+            fastq_record.trim_5p_Ts()
+            assert fastq_record.name == \
+                "TS17TATCTC::ATTTTTTTTTTTT:NB500952:186:H35F2BGX5:1:11101:14875:1080 1:N:0:ATCACG"
+            assert fastq_record.seq == \
+                "GAAGGGCAGATTTAAAATACACTATTAAAATTATTAAATATTAAAAAACAACA"
+            assert fastq_record.qual == \
+                "AAE///E///E</EE/A/EE</6//6//<66//6//6//////<//6EE/6/6"
+
+    
+
+ 
