69838d707cf6af3a43b850e4eac8f4d554bda8d4
DinghaiZ <dinghaiz@gmail.com>
2019-02-02 09:29:26 -0500

    Simplified the __str__ method in the Fastq class and added test for it

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -12,13 +12,19 @@ def test_fastq_reader():
     for i, fastq_record in enumerate(PASS.fastq_reader(infile, 6, 4)):
         if i == 1:
             assert fastq_record.randNT5 == 6
-
+            assert str(fastq_record) == "@NB500952:186:H35F2BGX5:1:11101:1487"\
+                + "5:1080 1:N:0:ATCACG\nTATCTCTTTTATTTTTTTTTTTTGAAGGGCAGATTTA"\
+                    + "AAATACACTATTAAAATTATTAAATATTAAAAAACAACA\n+\nAAA/A/EEEE"\
+                        + "EEEEEEEEEAEE/AAE///E///E</EE/A/EE</6//6//<66//6//6"\
+                            + "//////<//6EE/6/6"
             assert fastq_record.name == \
                 "NB500952:186:H35F2BGX5:1:11101:14875:1080 1:N:0:ATCACG"
             assert fastq_record.seq == \
-                "TATCTCTTTTATTTTTTTTTTTTGAAGGGCAGATTTAAAATACACTATTAAAATTATTAAATATTAAAAAACAACA"
+                "TATCTCTTTTATTTTTTTTTTTTGAAGGGCAGATTTAAAATACACTATTAAAATTATTAA"\
+                    + "ATATTAAAAAACAACA"
             assert fastq_record.qual == \
-                "AAA/A/EEEEEEEEEEEEEAEE/AAE///E///E</EE/A/EE</6//6//<66//6//6//////<//6EE/6/6"
+                "AAA/A/EEEEEEEEEEEEEAEE/AAE///E///E</EE/A/EE</6//6//<66//6//6/"\
+                    + "/////<//6EE/6/6"
     
     assert i == 249
 
@@ -27,6 +33,7 @@ def test_trim_5p_Ts():
     for i, fastq_record in enumerate(PASS.fastq_reader(infile, 6, 4)):
         # Trim once
         if i == 2:
+            print(fastq_record)
             fastq_record.trim_5p_Ts()
             assert fastq_record.name == \
                 "TS16AACCAT:NB500952:186:H35F2BGX5:1:11101:13764:1083 1:N:0:ATCACG"
@@ -38,7 +45,8 @@ def test_trim_5p_Ts():
         if i == 1:
             fastq_record.trim_5p_Ts()
             assert fastq_record.name == \
-                "TS17TATCTC::ATTTTTTTTTTTT:NB500952:186:H35F2BGX5:1:11101:14875:1080 1:N:0:ATCACG"
+                "TS17TATCTC::ATTTTTTTTTTTT:NB500952:186:H35F2BGX5:1:11101:148"\
+                    + "75:1080 1:N:0:ATCACG"
             assert fastq_record.seq == \
                 "GAAGGGCAGATTTAAAATACACTATTAAAATTATTAAATATTAAAAAACAACA"
             assert fastq_record.qual == \
