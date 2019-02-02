5a1881cbad231b6f71d1b11dee7258056b0f74d2
DinghaiZ <dinghaiz@gmail.com>
2019-02-02 12:47:07 -0500

    Added read_num, randNT5, and randNT3 as class variables of Fastq

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -16,7 +16,7 @@ def test_fastq_reader():
                 + "5:1080 1:N:0:ATCACG\nTATCTCTTTTATTTTTTTTTTTTGAAGGGCAGATTTA"\
                     + "AAATACACTATTAAAATTATTAAATATTAAAAAACAACA\n+\nAAA/A/EEEE"\
                         + "EEEEEEEEEAEE/AAE///E///E</EE/A/EE</6//6//<66//6//6"\
-                            + "//////<//6EE/6/6"
+                            + "//////<//6EE/6/6\n"
             assert fastq_record.name == \
                 "NB500952:186:H35F2BGX5:1:11101:14875:1080 1:N:0:ATCACG"
             assert fastq_record.seq == \
@@ -26,14 +26,24 @@ def test_fastq_reader():
                 "AAA/A/EEEEEEEEEEEEEAEE/AAE///E///E</EE/A/EE</6//6//<66//6//6/"\
                     + "/////<//6EE/6/6"
     
-    assert i == 249
+    assert i == fastq_record.read_num - 1
 
 def test_trim_5p_Ts():
     infile = "data/rawfastq/siCtrl_1_small.fastq"
     for i, fastq_record in enumerate(PASS.fastq_reader(infile, 6, 4)):
+        # No trimming
+        if i == 0:
+            fastq_record.trim_5p_Ts()
+            assert fastq_record.name == \
+                "NB500952:186:H35F2BGX5:1:11101:6050:1064 1:N:0:ATCACG"
+            assert fastq_record.seq == \
+                "GAGGTCCGCGCGCCCCCCTGTATAGAAATCAACATTCTCTCCCAGAATTCTGTATATCTGT"\
+                    + "AACTATAGAAATGTC"
+            assert fastq_record.qual == \
+                "AAAAAEEEEAAEEEEEEE/E/E/A/E6E/AAE/E/</</AEE/6/////A<//<A////6/"\
+                    + "//6/A</EEEAA/<6"
         # Trim once
         if i == 2:
-            print(fastq_record)
             fastq_record.trim_5p_Ts()
             assert fastq_record.name == \
                 "TS16AACCAT:NB500952:186:H35F2BGX5:1:11101:13764:1083 1:N:0:ATCACG"
